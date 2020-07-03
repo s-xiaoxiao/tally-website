340fb3710f349ddcf9498a2395749d66d3b69cc6
xiaoxiao <460046653@qq.com>
2020-07-03 14:00:28 +0800

    deploy

---
+++ precache-manifest.2a509e0252eb72a695e387ffe88ff580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1957faeec7379bae98",
+    "url": "/tally-website/css/app.1ce96996.css"
+  },
+  {
+    "revision": "c7f4dc61ac7b91fb1f4323c483d61acf",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "8f1957faeec7379bae98",
+    "url": "/tally-website/js/app.0f771c2a.js"
+  },
+  {
+    "revision": "7ce17d5c4876b5c8b346",
+    "url": "/tally-website/js/chunk-vendors.e9fa2ce0.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "91fa880b64e30718ddb30c41b465caf0",
+    "url": "/tally-website/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tally-website/robots.txt"
+  }
+]);
